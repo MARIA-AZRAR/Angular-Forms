51615f4c57947e3ec6a55ff3829dd8e4331247b4
MariaAzrar <mariaazrar2000@gmail.com>
2020-05-30 01:57:45 +0500

    03. Creating a Data Model

--- src/app/user-setting-form/user-setting-form.component.ts
+++ src/app/user-setting-form/user-setting-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserSettings } from '../data/user-settings-form';
 
 @Component({
   selector: 'app-user-setting-form',
@@ -7,8 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserSettingFormComponent implements OnInit {
 
+  userSettings: UserSettings =  {
+    name:"Milton",
+    emailOffers: true,
+    interfaceStyle: "Light",
+    subscriptionType: "Monthly",            //we have created an object with predefined properties
+    notes: "Some notes"
+  }
   constructor() { }
-
   ngOnInit(): void {
   }
 
