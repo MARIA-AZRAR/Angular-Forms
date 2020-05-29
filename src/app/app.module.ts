f3ce2252925c9adb0e6112685a644020b93a205f
MariaAzrar <mariaazrar2000@gmail.com>
2020-05-30 01:17:06 +0500

    ng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms'
+import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { UserSettingFormComponent } from './user-setting-form/user-setting-form.component';
@@ -12,7 +13,8 @@ import { UserSettingFormComponent } from './user-setting-form/user-setting-form.
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
