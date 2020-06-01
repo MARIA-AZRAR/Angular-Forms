8d8e0718ab1945dfa495812d7e8a8868c4f42ff7
MariaAzrar <mariaazrar2000@gmail.com>
2020-06-02 04:16:55 +0500

    a data service has been created

---
+++ src/app/data/data.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { UserSettings } from './user-settings-form';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor() { }
+
+  postUserSettingForm(userSettings: UserSettings) {
+
+  }
+
+}
