0960319f81462b41df7c246617286c03cf597872
MariaAzrar <mariaazrar2000@gmail.com>
2020-06-03 02:58:07 +0500

    form post

--- src/app/data/data.service.ts
+++ src/app/data/data.service.ts
@@ -12,9 +12,16 @@ export class DataService {
 
   postUserSettingForm(userSettings: UserSettings): Observable <any> {   //http return type is any
     
-    return this.http.post('URL', userSettings);      //http can be returned as observable
-     
+    return this.http.post('https://putsreq.com/W7nLfT6MHxFdqXY0U0u4', userSettings);  //URl from putsreq //http can be returned as observable
+
     //return of(userSettings);
   }
 
 }
+
+/*   PutsReq means a little server Side Code
+var parsedBody = JSON.parse(request.body);
+parsedBody.id = '123';
+response.body = parsedBody;
+response.status = 200;
+*/
